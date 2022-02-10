ffaf7e5e01b6ae8ed23f6f8bd96a882e57b29f64
Elrabu <49643239+Elrabu@users.noreply.github.com>
2022-02-10 19:42:01 +0100

    AdventureGame-PlayerHealthBar
    Added a Health Bar for the showing the Players Helath + takes Damage from Mushroom Enemy

---
+++ AdventureGame - PlayerHealthBar/Assets/PlayerAttack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+    
+
+    private void Update()
+    {
+
+        if(timeBtwAttack <= 0){
+
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                
+                timeBtwAttack = startTimeBtwAttack;
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    CinemachineShake.Instance.ShakeCamera(5f, .1f);
+                    enemiesToDamage[i].GetComponent<PatrolNew>().TakeDamage(damage);
+                }
+            }
+
+
+            
+        } else {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
