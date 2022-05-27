ff4021953c44ac6481cce6433e37c91baf51a3fe
Elrabu <49643239+Elrabu@users.noreply.github.com>
2022-05-27 14:53:42 +0200

    AdventureGame - RandomEnemySpawn02
    Now Monsters that are randomly spawned can damage the player.

---
+++ AdventureGame - RandomEnemySpawn02/Assets/GameAssets.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets _i;
+    public static GameAssets I
+    {
+        get{
+            if (_i == null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    public Transform pfDamagePopup;
+}
---
+++ AdventureGame - RandomEnemySpawn02/Assets/RandomSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
+        }
+    }
+}
